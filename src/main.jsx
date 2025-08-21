674fd27503688c34073fa07e120bbca2f1386961
Juan Sebastian Vega <sebaschanvega@gmail.com>
2025-08-20 23:38:28 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
-import { CartProvider } from "./context/CartContext"; // ⟵ IMPORTANTE
+import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
-      {" "}
-      {/* ⟵ ENVUELVE TODA LA APP */}
       <App />
     </CartProvider>
   </React.StrictMode>
