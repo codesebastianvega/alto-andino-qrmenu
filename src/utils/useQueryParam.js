2ff648f99a8f27e958b8b0fe908b0cb4bf721c99
Juan Sebastian Vega <106327470+codesebastianvega@users.noreply.github.com>
2025-08-21 00:18:59 -0500

    feat: handle server side query param

--- src/utils/useQueryParam.js
+++ src/utils/useQueryParam.js
@@ -1,7 +1,10 @@
 import { useMemo } from "react";
+
 export function useQueryParam(name) {
-  return useMemo(
-    () => new URLSearchParams(window.location.search).get(name),
-    [name]
-  );
+  return useMemo(() => {
+    if (typeof window === "undefined") {
+      return null;
+    }
+    return new URLSearchParams(window.location.search).get(name);
+  }, [name, typeof window !== "undefined" ? window.location.search : null]);
 }
