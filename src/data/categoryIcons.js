312fe33fd5dbe78d0116b6b5afc8d569d625ebdd
Juan Sebastian Vega <106327470+codesebastianvega@users.noreply.github.com>
2025-08-22 18:03:37 -0500

    feat: add category icons to bar

---
+++ src/data/categoryIcons.js
@@ -0,0 +1,10 @@
+export const categoryIcons = {
+  desayunos: "fluent-emoji:croissant",
+  bowls: "fluent-emoji:pot-of-food",
+  "platos-fuertes": "fluent-emoji:fork-and-knife-with-plate",
+  sandwiches: "fluent-emoji:sandwich",
+  smoothies: "fluent-emoji:tropical-drink",
+  cafes: "fluent-emoji:hot-beverage",
+  postres: "fluent-emoji:shortcake",
+  "bebidas-frias": "fluent-emoji:ice",
+};
