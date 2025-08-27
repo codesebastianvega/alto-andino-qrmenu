88f6bfa7572c0e3afe528428f8edab110d8ec2ed
Juan Sebastian Vega <sebaschanvega@gmail.com>
2025-08-26 23:44:04 -0500

    Delete images.js

--- src/data/images.js
+++
@@ -1,5 +0,0 @@
-export const productImages = {
-  // Reemplaza estos ejemplos con tus URLs reales en /public/images/...
-  // "sandwich-cerdo": "/images/sandwich-cerdo.webp",
-  // "cumbre-energetica": "/images/cumbre-energetica.webp",
-};
