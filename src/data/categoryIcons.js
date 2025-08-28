9541bceea9f64729c8a9115c4c13b96895367669
Juan Sebastian Vega <sebaschanvega@gmail.com>
2025-08-28 16:35:46 -0500

    Update categoryIcons.js

--- src/data/categoryIcons.js
+++ src/data/categoryIcons.js
@@ -1,17 +1,12 @@
 export const categoryIcons = {
   todos: "ph:squares-four",
   desayunos: "fluent-emoji:croissant",
-  bowls: "fluent-emoji:pot-of-food",
+  bowls: "noto:bento-box",
   platos: "fluent-emoji:fork-and-knife-with-plate",
   sandwiches: "fluent-emoji:sandwich",
-  smoothies: {
-    icon: "fluent-emoji:cup-with-straw",
-    fallback: "noto:bubble-tea",
-  },
-  cafe: {
-    icon: "fluent-emoji:hot-beverage",
-    fallback: "noto:hot-beverage",
-  },
+  smoothies: "noto:cup-with-straw",
+  cafe: "noto:hot-beverage",
+  veggie: "fluent-emoji:leafy-green",
   postres: "fluent-emoji:shortcake",
   bebidasfrias: "fluent-emoji:ice",
 };
